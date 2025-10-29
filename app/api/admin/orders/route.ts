49db82a17af2b08bf79de292eb70cbba8648ee24
Belousov Dmitriy <d-belousov@hotmail.com>
2025-10-29 22:17:04 +0500

    FIRST COMMIT
    * создаст бекенд

---
+++ app/api/admin/orders/route.ts
@@ -0,0 +1,34 @@
+import {NextRequest, NextResponse} from "next/server";
+import fs from "fs";
+import path from "path";
+
+type Order = {
+  "id": string,
+  "route": string,
+  "date": string,
+  "passenger": string,
+  "amount": number,
+  "status": "PENDING" | "PAID" | "CANCELED"
+}
+
+export async function GET(req: NextRequest) {
+  console.log('GET');
+  const filePath = path.join(process.cwd(), "data", "mock-orders.json");
+  const data = fs.readFileSync(filePath, "utf-8");
+  const orders = JSON.parse(data);
+  console.log(orders, 'OPRDERS');
+  try {
+
+    const status = req.nextUrl.searchParams.get("status");
+    const result = status ? orders.filter((order: Order) => order.status === status) : orders;
+    return NextResponse.json(result);
+  } catch (e) {
+    return NextResponse.json({
+      error: "Failed to fetch orders",
+      status: 500,
+    })
+
+  }
+}
+
+
